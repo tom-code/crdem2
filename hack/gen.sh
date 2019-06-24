093fc95bc851373a19ed327c3f0e5b75366b91a7
tom <tom@mm.lan>
2019-06-24 23:20:42 +0200

    gomod

--- hack/gen.sh
+++ hack/gen.sh
@@ -1,2 +1,2 @@
 
-vendor/k8s.io/code-generator/generate-groups.sh all crdem/pkg/apis/generated crdem/pkg/apis "svc1:v1"
+../../pkg/mod/k8s.io/code-generator\@v0.0.0-20190311093542-50b561225d70/generate-groups.sh all crdem/pkg/apis/generated crdem/pkg/apis "svc1:v1"
