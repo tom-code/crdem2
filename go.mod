01cccf569396c8ab5be0f813e37234dc4b0a2f3e
tom <petrilak.tomas@gmail.com>
2019-11-16 12:33:02 +0100

    learn to create deployment

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module a.com/crdem
 go 1.12
 
 require (
+	k8s.io/api v0.0.0-20190620084959-7cf5895f2711
 	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
 	k8s.io/client-go v12.0.0+incompatible
 	k8s.io/code-generator v0.0.0-20190912054826-cd179ad6a269 // indirect
--- t1.go
+++ t1.go
@@ -9,7 +9,56 @@ import (
   muj "a.com/crdem/pkg/apis/generated/clientset/versioned"
   "k8s.io/client-go/tools/cache"
   mj "a.com/crdem/pkg/apis/svc1/v1"
+  appsv1 "k8s.io/api/apps/v1"
+  corev1 "k8s.io/api/core/v1"
+
 )
+
+var (
+  clientset *kubernetes.Clientset
+)
+
+func createDepl() {
+  replicas := int32(1)
+  dep := &appsv1.Deployment{
+    ObjectMeta: metav1.ObjectMeta {
+      Name: "t1",
+      Namespace: "default",
+      Annotations: map[string]string {"a": "1"},
+    },
+    Spec: appsv1.DeploymentSpec {
+      Replicas: &replicas,
+      Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string {"sel": "abc"},
+      },
+      Template: corev1.PodTemplateSpec{
+        ObjectMeta: metav1.ObjectMeta{
+					Labels:      map[string]string {"sel": "abc"},
+					Annotations: map[string]string {"a": "1"},
+        },
+        Spec: corev1.PodSpec{
+          Containers: []corev1.Container { {
+              Name: "cont1",
+              Image: "10.10.2.213:5000/amf-eefat:iB4.0.1",
+              Command: []string{"/bin/sleep"},
+              Args: []string{"1000"},
+              Env: []corev1.EnvVar {
+                corev1.EnvVar { Name: "E1", Value: "V1" },
+              },
+            },
+          },
+        },
+      },
+    },
+  }
+  dep2, err := clientset.AppsV1().Deployments("default").Create(dep)
+  fmt.Println(dep2)
+  fmt.Println(err)
+}
+
+func DeleteDepl() {
+  clientset.AppsV1().Deployments("default").Delete("t1", &metav1.DeleteOptions{})
+}
 
 func main() {
   config, err := clientcmd.BuildConfigFromFlags("", "admin.conf")
@@ -18,7 +67,7 @@ func main() {
   }
 
   //clientset for core stuff
-  clientset, err := kubernetes.NewForConfig(config)
+  clientset, err = kubernetes.NewForConfig(config)
   if err != nil {
     panic(err.Error())
   }
@@ -43,7 +92,7 @@ func main() {
   
   fmt.Println("----list extension objects")
   s := cs.Svc1V1()
-  zz := s.TestTypes("default")
+  zz := s.TestTypes("")
   lst, err := zz.List(metav1.ListOptions{})
   if err != nil {
     panic(err.Error())
@@ -86,12 +135,14 @@ func main() {
       cache.ResourceEventHandlerFuncs{
         AddFunc: func(obj interface{}) {
           fmt.Printf("added: %s \n", obj)
+          createDepl()
         },
         DeleteFunc: func(obj interface{}) {
           fmt.Printf("deleted: %s \n", obj)
+          DeleteDepl()
         },
         UpdateFunc: func(oldObj, newObj interface{}) {
-          fmt.Printf("changed \n")
+          fmt.Printf("changed %s\n", newObj)
         },
       },
   )
